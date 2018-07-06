514dc4d811d92544e03406240b04d3b5fbab5fcf
Cole Dean Shepherd <cole@coledeanshepherd.com>
2018-07-06 00:09:23 -0700

    Added a simple parser & interpreter.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,10 @@
 import Data.Char
 -- use Data.Text instead of [Char]!
 
+-- Lexer
 data Token =
     Identifier String
-  | Number String
+  | NumberToken String
   | LeftParen String
   | RightParen String
   | Comma String
@@ -28,8 +29,80 @@ readIdentifier str =
 readNumber :: String -> (Token, String)
 readNumber str =
   let (tokenText, str') = span isDigit str in
-    (Number tokenText, str')
+    (NumberToken tokenText, str')
+
+-- Parser
+data ParseTree =
+  IdentifierNode String
+  | NumberNode String
+  | FunctionCall ParseTree [ParseTree]
+  deriving (Show)
+
+parse :: [Token] -> ParseTree
+parse tokens =
+  let (expr, tokens') = parseExpression tokens in
+    case tokens' of
+      [] -> expr
+      _ -> error "Expected the end of the source code."
+
+parseExpression :: [Token] -> (ParseTree, [Token])
+parseExpression tokens =
+  let (prefixExpr, tokens') = parsePrefixExpression tokens in
+    parseInfixExpression prefixExpr tokens'
+
+parsePrefixExpression :: [Token] -> (ParseTree, [Token])
+parsePrefixExpression (t:ts) =
+  case t of
+    Identifier text -> (IdentifierNode text, ts)
+    NumberToken text -> (NumberNode text, ts)
+    _ -> error ("Unknown token: " ++ (show t))
+
+parseInfixExpression :: ParseTree -> [Token] -> (ParseTree, [Token])
+parseInfixExpression prefixExpr (t:ts) =
+  case t of
+    LeftParen _ -> parseFunctionCall prefixExpr (t:ts)
+    _ -> (prefixExpr, t:ts)
+
+parseFunctionCall :: ParseTree -> [Token] -> (ParseTree, [Token])
+parseFunctionCall funcExpr tokens =
+  let (args, t') = parseFunctionCallArgumentsTuple tokens in
+    (FunctionCall funcExpr args, t')
+
+parseFunctionCallArgumentsTuple :: [Token] -> ([ParseTree], [Token])
+parseFunctionCallArgumentsTuple (t:ts) =
+  case t of
+    LeftParen _ ->
+      let (args, (t':ts')) = parseCommaSeparated parseExpression ts in
+        case t' of
+          RightParen text -> (args, ts')
+          _ -> error ("Expected ')' but encountered" ++ (show t'))
+    _ -> error ("Expected '(' but encountered: " ++ (show t))
+
+parseCommaSeparated :: ([Token] -> (ParseTree, [Token])) -> [Token] -> ([ParseTree], [Token])
+parseCommaSeparated parseFunc tokens =
+  let (parseTree, (t':ts')) = parseFunc tokens in
+    case t' of
+      Comma _ ->
+        let (parseTreesTail, t'') = parseCommaSeparated parseFunc ts' in
+          (parseTree:parseTreesTail, t'')
+      _ -> ([parseTree], t':ts')
+
+-- Interpreter
+evaluate :: ParseTree -> Double
+evaluate (FunctionCall funcExpr args) =
+  case funcExpr of
+    IdentifierNode text ->
+      case text of
+        "add" -> sum (map evaluate args)
+        _ -> error ("Unsupported function: " ++ text)
+    _ -> error ("Unsupported function expression type: " ++ (show funcExpr))
+evaluate (NumberNode text) = read text::Double
 
 sourceCode = "add(1, 2)"
 
-main = print (tokenize sourceCode)
+main =
+  let tokens = tokenize sourceCode in
+    let parseTree = parse tokens in
+      do
+        print parseTree
+        print (evaluate parseTree)
