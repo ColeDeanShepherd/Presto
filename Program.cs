363391a24f761939b330ddc835a0b591e4d43dcc
Cole Dean Shepherd <cole@coledeanshepherd.com>
2019-06-25 00:06:32 -0700

    Fixed last commit.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Presto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printHelloWorld = new FunctionCall
+            {
+                FunctionExpression = new MemberAccessOperator
+                {
+                    MemberContainer = new MemberAccessOperator
+                    {
+                        MemberContainer = new MemberAccessOperator
+                        {
+                            MemberContainer = new Identifier
+                            {
+                                Text = "std"
+                            },
+                            MemberIdentifier = new Identifier
+                            {
+                                Text = "io"
+                            }
+                        },
+                        MemberIdentifier = new Identifier
+                        {
+                            Text = "stdout"
+                        }
+                    },
+                    MemberIdentifier = new Identifier
+                    {
+                        Text = "writeLine"
+                    }
+                },
+                Arguments = new List<IExpression>
+                {
+                    new StringLiteral
+                    {
+                        Value = "Hello, world!"
+                    }
+                }
+            };
+            var ast = new FunctionDefinition
+            {
+                Name = new Identifier
+                {
+                    Text = "main"
+                },
+                Body = new Block
+                {
+                    Statements = new List<IStatement>
+                    {
+                        printHelloWorld
+                    }
+                }
+            };
+
+            var prestoCodeGenerator = new PrestoCodeGenerator();
+            prestoCodeGenerator.Visit(ast);
+
+            Console.WriteLine(prestoCodeGenerator.GeneratedCode);
+            Console.ReadKey();
+        }
+    }
+}
