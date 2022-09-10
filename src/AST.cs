d42791682c6e48dd22c3b8cb706f635c09820894
Cole Shepherd <cole@recroom.com>
2022-09-10 09:12:27 -0700

    Creating AST for hello world program.

---
+++ src/AST.cs
@@ -0,0 +1,24 @@
+﻿namespace Presto.AST
+{
+    public interface IDeclaration { }
+
+    public record Namespace(
+        string Name,
+        List<IDeclaration> Declarations
+    ) : IDeclaration;
+
+    public record Function(
+        string Name
+    ) : IDeclaration;
+
+    public interface IExpression { }
+
+    public record FunctionCall(
+        Function Function,
+        List<IExpression> Arguments
+    ) : IExpression;
+
+    public record StringLiteral(
+        string Value
+    ) : IExpression;
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Presto.AST;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create "Console" namespace.
+        Namespace consoleNamespace = new(
+            "Console",
+            Declarations: new List<IDeclaration>());
+
+        // Create "WriteLine" function.
+        Function writeLineFunction = new("WriteLine");
+
+        // Add "WriteLine" function to "Console" namespace.
+        consoleNamespace.Declarations.Add(writeLineFunction);
+
+        // Create program AST.
+        List<IExpression> program = new()
+        {
+            new FunctionCall(
+                writeLineFunction,
+                Arguments: new List<IExpression>
+                {
+                    new StringLiteral("Hello, World!")
+                })
+        };
+
+        // Output to console.
+        Console.WriteLine("Not there yet!");
+    }
+}
