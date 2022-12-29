f9db3b92168450fc009ddbcd8d339e76c2882a25
Cole Shepherd <cole@recroom.com>
2022-12-29 02:01:24 -0800

    Refactoring.

--- app/Main.hs
+++ app/Main.hs
@@ -12,16 +12,21 @@ add = (a, b) -> a + b
 sourceCode :: String
 sourceCode = "id = (x) -> x"
 
+compile :: String -> (String, [String])
+compile sourceCode =
+  let parseResult = parsePrestoProgram sourceCode in
+    case parseResult of
+      Right bindings -> (generateCode bindings, [])
+      Left e -> ("", [show e])
+
 main :: IO ()
 main = do
   putStrLn "Source code:"
   putStrLn sourceCode
-
   putStrLn ""
 
-  let parseResult = parsePrestoProgram sourceCode in
-    case parseResult of
-      Right bindings -> do
-        putStrLn "Generated code:"
-        putStrLn $ generateCode bindings
-      Left e -> print ("Parse error: " ++ show e)
+  let (generatedCode, errors) = compile sourceCode in do
+    mapM_ putStrLn errors
+
+    putStrLn "Generated code:"
+    putStrLn generatedCode
