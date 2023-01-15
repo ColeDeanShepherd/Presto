aa133a7ff9223cd64a9c9ca7c4f85181112977d3
Cole Shepherd <cole@recroom.com>
2023-01-15 09:21:49 -0800

    Added a code generator.

---
+++ CodeGenerator.fs
@@ -0,0 +1,76 @@
+﻿module CodeGenerator
+
+open ASTBuilder
+open CompilerCore
+
+type CodeGeneratorOutput = {
+    GeneratedCode: string
+    Errors: List<CompileError>
+}
+
+let generateString (state: CodeGeneratorOutput) (str: string): CodeGeneratorOutput =
+    { state with GeneratedCode = state.GeneratedCode + str }
+
+let rec generateMany
+    (state: CodeGeneratorOutput)
+    (nodes: List<'a>)
+    (generateFn: (CodeGeneratorOutput -> 'a -> CodeGeneratorOutput))
+    (separator: string)
+    (isFirstItem: bool)
+    : CodeGeneratorOutput =
+        if nodes.IsEmpty then
+            state
+        else
+            let state =
+                if not isFirstItem then
+                    generateString state separator
+                else
+                    state
+
+            let state = generateFn state nodes.Head
+
+            generateMany state nodes.Tail generateFn separator false
+
+let generateSymbol (state: CodeGeneratorOutput) (symbol: Symbol): CodeGeneratorOutput =
+    match symbol with
+    | BindingSymbol binding -> { state with GeneratedCode = state.GeneratedCode + binding.Name }
+    | ParameterSymbol parameter ->  { state with GeneratedCode = state.GeneratedCode + parameter.Name }
+
+let generateExpression (state: CodeGeneratorOutput) (expression: Expression): CodeGeneratorOutput =
+    match expression with
+    | FunctionExpression fn -> failwith "Not implemented"
+    | SymbolReference symbol -> generateSymbol state symbol
+
+let generateParameter (state: CodeGeneratorOutput) (parameter: Parameter): CodeGeneratorOutput =
+    let state = generateString state "int"
+    let state = generateString state " "
+    let state = generateString state parameter.Name
+
+    state
+
+let generateFunction (state: CodeGeneratorOutput) (name: string) (fn: Function): CodeGeneratorOutput =
+    let state = generateString state "int"
+    let state = generateString state " "
+    let state = generateString state name
+    let state = generateString state "("
+    let state = generateMany state fn.Parameters generateParameter ", " true
+    let state = generateString state ")"
+    let state = generateString state " "
+    let state = generateString state "{"
+    let state = generateString state "return"
+    let state = generateString state " "
+    let state = generateExpression state fn.Value
+    let state = generateString state ";"
+    let state = generateString state "}"
+
+    state
+
+let generateBinding (state: CodeGeneratorOutput) (binding: Binding): CodeGeneratorOutput =
+    match binding.Value with
+    | FunctionExpression fn -> generateFunction state binding.Name fn
+    | SymbolReference symbol -> generateSymbol state symbol
+
+let generateCode (program: Program): CodeGeneratorOutput =
+    let state = { GeneratedCode = ""; Errors = [] }
+
+    generateMany state program.Bindings generateBinding "\n" true
--- Program.fs
+++ Program.fs
@@ -2,26 +2,39 @@
 open Lexer
 open Parser
 open ASTBuilder
+open CodeGenerator
 
 let fileName = "../../../BootstrappedCompiler.pst"
 
 let sourceCode = File.ReadAllText fileName
+
+// tokenize
 let tokenizeOutput = tokenize sourceCode
 
 if not tokenizeOutput.Errors.IsEmpty then
     for error in tokenizeOutput.Errors do
          printfn $"{error}"
 else
+    // parse
     let parseOutput = parse tokenizeOutput.Tokens
 
     if not parseOutput.Errors.IsEmpty then
         for error in parseOutput.Errors do
              printfn $"{error}"
     else
+        // build AST
         let buildAstOutput = buildAst parseOutput.Program
 
         if not buildAstOutput.Errors.IsEmpty then
             for error in buildAstOutput.Errors do
                  printfn $"{error}"
         else
-            printfn "Building the AST succeeded."
+            // generate code
+            let codeGeneratorOutput = generateCode buildAstOutput.Program
+
+            if not codeGeneratorOutput.Errors.IsEmpty then
+                for error in codeGeneratorOutput.Errors do
+                     printfn $"{error}"
+            else
+                printfn "Compile succeeded!"
+                printfn $"{codeGeneratorOutput.GeneratedCode}"
