296ba917cbe79ec101cfb0f489aa3d14f119739a
Cole Shepherd <cole@recroom.com>
2023-04-11 21:14:43 -0700

    Calling OpenAI chat completion API.

---
+++ Presto.AI.Agents/OpenAI.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenAI
+{
+    public enum Model
+    {
+        GPT_3_5_Turbo
+    }
+
+    public static class Constants
+    {
+        public static readonly Dictionary<Model, string> IdStringsByModel = new()
+        {
+            { Model.GPT_3_5_Turbo, "gpt-3.5-turbo" }
+        };
+    }
+
+    public record ChatMessage(
+        [property: JsonPropertyName("role")]
+        string Role,
+
+        [property: JsonPropertyName("content")]
+        string Content);
+
+    public record ChatCompletionRequest(
+        [property: JsonPropertyName("model")]
+        string Model,
+
+        [property: JsonPropertyName("messages")]
+        ChatMessage[] Messages);
+
+    public record ChatCompletionChoice(
+        [property: JsonPropertyName("message")]
+        ChatMessage Message);
+
+    public record ChatCompletionResponse(
+        [property: JsonPropertyName("choices")]
+        ChatCompletionChoice[] Choices);
+
+
+    public static class API
+    {
+        public static async Task<ChatCompletionResponse> CreateChatCompletion(string apiKey, ChatCompletionRequest request)
+        {
+            using var httpClient = new HttpClient();
+
+            var dataJson = JsonSerializer.Serialize(request);
+            var content = new StringContent(dataJson, Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            var responseMessage = await httpClient.PostAsync("https://api.openai.com/v1/chat/completions", content);
+
+            var response = await responseMessage.Content.ReadFromJsonAsync<ChatCompletionResponse>();
+
+            if (response == null)
+            {
+                throw new Exception();
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Presto.AI.Agents/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI;
+
+namespace Presto.AI.Agents;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+          .AddUserSecrets<Program>()
+          .Build();
+
+        string apiKey = configuration["OpenAIAPIKey"] ?? "";
+
+        var request = new ChatCompletionRequest(
+            "gpt-3.5-turbo",
+            new[]
+            {
+                new ChatMessage("user", "Hello!")
+            });
+
+        ChatCompletionResponse response = await API.CreateChatCompletion(apiKey, request);
+        ChatMessage responseChatMessage = response.Choices[0].Message;
+    }
+}
