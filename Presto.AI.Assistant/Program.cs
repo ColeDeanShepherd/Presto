57a42760e715d18fec82632f4333246f09ff6454
Cole Shepherd <cole@recroom.com>
2023-05-03 09:29:03 -0700

    Refactoring.

--- Presto.AI.Assistant/Program.cs
+++ Presto.AI.Assistant/Program.cs
@@ -3,7 +3,27 @@ using System.Runtime.InteropServices;
 
 namespace Presto.AI.Assistant;
 
-interface IDesktopApps
+// TODO:
+// - non-singleton service lifetimes (scoped, transient?)
+// - thread-safety
+public struct Injected<TInterface>
+{
+    public static void Register(TInterface implementation)
+    {
+        _value = implementation;
+        _hasValue = true;
+    }
+
+    private static TInterface? _value;
+    private static bool _hasValue;
+
+    public TInterface Value =>
+        _hasValue
+            ? _value!
+            : throw new InvalidOperationException();
+}
+
+public interface IDesktopApps
 {
     void OpenApp(string fileName, string arguments = "");
     void OpenUrlInBrowser(string url);
@@ -35,7 +55,7 @@ interface IDesktopApps
         OpenUrlInBrowser("https://calendar.google.com/");
 }
 
-class DesktopApps : IDesktopApps
+public class DesktopApps : IDesktopApps
 {
     public void OpenApp(string fileName, string arguments = "")
     {
@@ -73,29 +93,45 @@ class DesktopApps : IDesktopApps
     }
 }
 
+public static class ColesCommands
+{
+    public static void OpenRecRoomDesktopEnvironment(Injected<IDesktopApps> desktopApps = default)
+    {
+        desktopApps.Value.OpenChrome();
+        desktopApps.Value.OpenYouTube();
+        OpenColesNotesDoc();
+        desktopApps.Value.OpenGmail();
+        desktopApps.Value.OpenGoogleCalendar();
+        OpenRecRoomAdoPullRequests();
+        desktopApps.Value.OpenSlack();
+        desktopApps.Value.OpenApp(GetRecNetDevEnvPath());
+        desktopApps.Value.OpenNotepadPlusPlus();
+    }
+
+    public static void OpenColesNotesDoc(Injected<IDesktopApps> desktopApps = default) =>
+        desktopApps.Value.OpenUrlInBrowser("https://docs.google.com/document/d/1IxN-PAExj77bW0WR1PcX_9LkDB193_sGGL0fAkwrYQ4/edit#");
+
+    public static void OpenRecRoomAdoPullRequests(Injected<IDesktopApps> desktopApps = default) =>
+        desktopApps.Value.OpenUrlInBrowser("https://dev.azure.com/recroom/_pulls");
+
+    public static string GetRecNetDevEnvPath() =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            @"dev\DevTools\RecNetDevEnv\bin\Release\RecNetDevEnv.exe");
+}
+
 internal class Program
 {
     static void Main(string[] args)
     {
         var desktopApps = new DesktopApps();
-        Run(desktopApps);
+        Injected<IDesktopApps>.Register(desktopApps);
+
+        Run();
     }
 
-    static void Run(IDesktopApps desktopApps)
+    static void Run(Injected<IDesktopApps> desktopApps = default)
     {
-        desktopApps.OpenChrome();
-        desktopApps.OpenYouTube();
-        desktopApps.OpenUrlInBrowser("https://docs.google.com/document/d/1IxN-PAExj77bW0WR1PcX_9LkDB193_sGGL0fAkwrYQ4/edit#"); // Cole's Notes
-        desktopApps.OpenGmail();
-        desktopApps.OpenGoogleCalendar();
-        desktopApps.OpenUrlInBrowser("https://dev.azure.com/recroom/_pulls");
-        desktopApps.OpenSlack();
-        desktopApps.OpenApp(GetRecNetDevEnvPath());
-        desktopApps.OpenNotepadPlusPlus();
+        ColesCommands.OpenRecRoomDesktopEnvironment();
     }
-
-    static string GetRecNetDevEnvPath() =>
-        Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-            @"dev\DevTools\RecNetDevEnv\bin\Release\RecNetDevEnv.exe");
 }
