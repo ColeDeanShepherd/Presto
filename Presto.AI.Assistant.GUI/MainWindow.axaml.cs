ad4d407d1b2f2abb1bc11fb791ed8a2ae5952f59
Cole Shepherd <cole@coledeanshepherd.com>
2024-07-10 21:55:04 -0700

    Add Avalonia GUI

---
+++ Presto.AI.Assistant.GUI/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Presto.AI.Assistant.GUI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
